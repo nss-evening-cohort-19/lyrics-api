cb345c1d90ad01ae895b6244933bda870adc8833
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2022-05-17 21:14:14 -0500

    Update main.js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,25 +1,25 @@
 // USE WITH FIREBASE AUTH
 // import checkLoginStatus from './helpers/auth';
+import axios from 'axios';
 import 'bootstrap'; // import bootstrap elements and js
 import '../styles/main.scss';
 
-const init = () => {
-  document.querySelector('#app').innerHTML = `
-    <h1>HELLO! You are up and running!</h1>
-    <small>Open your dev tools</small><br />
-    <button class="btn btn-danger" id="click-me">Click ME!</button><br />
-    <hr />
-    <h2>These are font awesome icons:</h2>
-    <i class="fas fa-user fa-4x"></i> <i class="fab fa-github-square fa-5x"></i>
-  `;
-  console.warn('YOU ARE UP AND RUNNING!');
+// API CALL
+const getLyrics = (artist, song) => new Promise((resolve, reject) => {
+  axios.get(`https://api.lyrics.ovh/v1/${artist}/${song}`)
+    .then((response) => resolve(response.data))
+    .catch((error) => reject(error));
+});
 
-  document
-    .querySelector('#click-me')
-    .addEventListener('click', () => console.warn('You clicked that button!'));
-
-  // USE WITH FIREBASE AUTH
-  // checkLoginStatus();
+// UI PRESENTATION (HTML ON THE DOM)
+const lyricsOnDom = (artist, song) => {
+  getLyrics(artist, song).then((response) => {
+    document.querySelector('#app').innerHTML = response.lyrics;
+  });
 };
 
-init();
+const startApp = () => {
+  lyricsOnDom('prince', 'purple rain');
+};
+
+startApp();
